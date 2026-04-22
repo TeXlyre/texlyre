69d759b5e0c5f90ce4bef2ed39aa0ccae27ab9c7
abawi <fares.abawi@uni-hamburg.de>
2026-04-22 14:54:17 +0200

    Commited and cleaned up final version of latex engine tests

--- tests/mocks/texlyre-busytex.js
+++ tests/mocks/texlyre-busytex.js
@@ -1,24 +1,27 @@
-const mockRunner = {
-    initialize: jest.fn(() => Promise.resolve()),
-    isInitialized: jest.fn(() => true),
-    terminate: jest.fn(),
-    compile: jest.fn(() =>
-        Promise.resolve({
-            success: true,
-            exitCode: 0,
-            log: '',
-            pdf: new Uint8Array(),
-            synctex: undefined,
-        })
-    ),
-    readProjectFiles: jest.fn(() => Promise.resolve([])),
-    writeTexliveRemoteMisses: jest.fn(() => Promise.resolve()),
-    writeTexliveRemoteFiles: jest.fn(() => Promise.resolve()),
-};
+function makeMockRunner() {
+    let initialized = false;
+    return {
+        initialize: jest.fn(() => { initialized = true; return Promise.resolve(); }),
+        isInitialized: jest.fn(() => initialized),
+        terminate: jest.fn(() => { initialized = false; }),
+        compile: jest.fn(() =>
+            Promise.resolve({
+                success: true,
+                exitCode: 0,
+                log: '',
+                pdf: new Uint8Array(),
+                synctex: undefined,
+            })
+        ),
+        readProjectFiles: jest.fn(() => Promise.resolve([])),
+        writeTexliveRemoteMisses: jest.fn(() => Promise.resolve()),
+        writeTexliveRemoteFiles: jest.fn(() => Promise.resolve()),
+    };
+}
 
 module.exports = {
     __esModule: true,
-    BusyTexRunner: jest.fn().mockImplementation(() => mockRunner),
+    BusyTexRunner: jest.fn().mockImplementation(() => makeMockRunner()),
     isPackageCached: jest.fn(() => Promise.resolve(false)),
     deletePackageCache: jest.fn(() => Promise.resolve()),
 };
