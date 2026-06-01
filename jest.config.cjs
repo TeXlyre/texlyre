2be6f6e502e5849dc45825d3caf81ed0394e0936
abawi <fares.abawi@uni-hamburg.de>
2026-06-01 10:07:00 +0200

    Added chelys protocol and service layer with WebAuthn helpers

--- jest.config.cjs
+++ jest.config.cjs
@@ -11,6 +11,7 @@ module.exports = {
 	moduleNameMapper: {
 		'^@/(.*)$': '<rootDir>/src/$1',
 		'^@src/(.*)$': '<rootDir>/src/$1',
+		'^@chelys/(.*)$': '<rootDir>/chelys/$1',
 		'^@extras/(.*)$': '<rootDir>/extras/$1',
 		'^@tests/(.*)$': '<rootDir>/tests/$1',
 		'\\.(css|less|scss|sass)$': 'identity-obj-proxy',
