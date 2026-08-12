0ce5ecfb10a123bdc9577512bb16f8994c1cef18
Fares Abawi <f.abawi@outlook.com>
2026-08-12 18:07:31 +0000

    Fixed texlyre-busytex remote paths

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 // These constants are automatically generated. Do not edit directly.
-// Generated on: 2026-08-11T21:54:45.689Z
-const CACHE_NAME = `texlyre-v0.10.14`;
+// Generated on: 2026-08-12T17:57:52.280Z
+const CACHE_NAME = `texlyre-v0.10.15`;
 const BASE_PATH = '/texlyre/';
 const FONTS_CACHE_NAME = 'fonts-cache-v1';
 const AIRGAP_ALLOWED_DOMAINS = [
