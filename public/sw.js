b9c2f7ed0e109b61415265acb48e9ed1f74e1d15
fabawi <f.abawi@outlook.com>
2026-08-19 15:55:37 +0100

    BUGFIX: Outlines showed annotation tags

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // These constants are automatically generated. Do not edit directly.
-// Generated on: 2026-08-19T12:36:16.568Z
+// Generated on: 2026-08-19T14:53:47.798Z
 const CACHE_NAME = `texlyre-v0.10.18`;
 const BASE_PATH = '/texlyre/';
 const FONTS_CACHE_NAME = 'fonts-cache-v1';
